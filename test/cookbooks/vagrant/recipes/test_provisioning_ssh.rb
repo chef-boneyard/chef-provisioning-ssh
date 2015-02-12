77cb9ae07310de758b4b2175e5b0d037237ffdbd
double-z <zackzondlo@gmail.com>
2015-02-12 11:46:45 -0500

    added chef_gem install to test_provisioning_ssh recipe

--- test/cookbooks/vagrant/recipes/test_provisioning_ssh.rb
+++ test/cookbooks/vagrant/recipes/test_provisioning_ssh.rb
@@ -37,7 +37,7 @@ machine "sshtwo" do
     'ip_address' => '192.168.33.23',
     'username' => 'vagrant',
     'ssh_options' => {
-      'keys' => ['~/.vagrant.d/insecure_private_key']
+      'keys' => ["#{ENV['HOME']}/.vagrant.d/insecure_private_key"]
     }
   }
   recipe 'vagrant::sshtwo'
