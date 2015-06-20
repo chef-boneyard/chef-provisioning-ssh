20f4ef9cbaa0477067bcbb18cca846f2fcedf2b2
Carsten Zeumer <carsten.zeumer@3drights.de>
2015-06-20 16:28:48 +0200

    Allow key_data parameter for SSH keys, avoid overwriting command prefix
    key_data added as an additional parameter for specifying SSH keys.
    Additionally, the command prefix is only set if it doesn't already exist,
    allowing a user to explicitly set it as an empty string or something of
    their choosing when not connecting as root. This includes a different
    method of avoiding the override from PR #20

--- lib/chef/provisioning/ssh_driver/driver.rb
+++ lib/chef/provisioning/ssh_driver/driver.rb
@@ -390,7 +390,7 @@ class Chef
           unless machine_options['transport_options']['is_windows']
             machine_options['transport_options']['options'] ||= {}
             unless machine_options['transport_options']['username'] == 'root'
-              machine_options['transport_options']['options']['prefix'] = 'sudo '
+              machine_options['transport_options']['options']['prefix'] ||= 'sudo '
             end
           end
           ensure_has_keys_or_password(machine_options['transport_options'])
@@ -405,9 +405,11 @@ class Chef
             if transport_hash['ssh_options']
               ssh_hash = transport_hash['ssh_options']
               keys = ssh_hash['keys'] || false
+              key_data = ssh_hash['key_data'] || false
               password = ssh_hash['password'] || false
               has_either = ((password && password.kind_of?(String)) ||
-                            (keys && !keys.empty? && keys.kind_of?(Array)))
+                            (keys && !keys.empty? && keys.kind_of?(Array)) ||
+                            (key_data && !key_data.empty? && key_data.kind_of?(Array)))
             else
               has_either = false
             end
